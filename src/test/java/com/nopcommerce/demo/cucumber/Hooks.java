1df974a00c3b9d2f723f1af3f26b352710fcb410
Primetraineesvn <primetraineesvn@gmail.com>
2022-05-13 19:48:22 +0100

    Cucumber project skeleton

---
+++ src/test/java/com/nopcommerce/demo/cucumber/Hooks.java
@@ -0,0 +1,12 @@
+package com.nopcommerce.demo.cucumber;
+
+import com.nopcommerce.demo.utility.Utility;
+
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Hooks extends Utility {
+
+
+}
---
+++ src/test/java/com/nopcommerce/demo/cucumber/runner/TestRunner.java
@@ -0,0 +1,9 @@
+package com.nopcommerce.demo.cucumber.runner;
+
+/**
+ * Created by Jay Vaghani
+ */
+
+public class TestRunner {
+
+}
